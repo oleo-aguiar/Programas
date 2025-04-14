6607f1109627a08bb11d167aaf5834e8ffe93f29
Leonardo <leo.aguiarbr@gmail.com>
2025-04-14 13:24:25 -0300

    Add files via upload

---
+++ ManipulacaoeConversao4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+int a = 7;
+int b = 2;
+int quociente = a / b; // Note que a divisão de 7 por 2 resulta em 3 pois a a parte decimal é descartada
+
+printtf("Quociente: %d\n", quociente);
+
+return 0;
+
+}
